6fc7c1f907acb34540b3b774b22d020ca2f48a1d
Josh Wroe <josh_wroe@hotmail.co.uk>
2018-09-25 09:26:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-function updateObjectWithKeyAndValue(object, key, value){
-  
+var recipes = new Object();
+
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
 }
